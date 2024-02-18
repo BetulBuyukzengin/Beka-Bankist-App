4eda2b34c7f4ed310a98e420771afc1b7911f029
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-02-19 00:02:41 +0300

    feat: add gallery and modal component

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,41 +1,79 @@
 import styled from "styled-components";
+import Heading from "../Heading";
+import SearchIcon from "@mui/icons-material/Search";
+import Modal from "../Modal/Modal";
+import { useState } from "react";
 
 const StyledGaleryContainer = styled.div`
-  height: 100dvh;
   padding: 5rem;
+  background-color: var(--color-background);
 `;
 const StyledGridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  gap: 16px;
-  /* height: 100vh; */
+  gap: 25px;
 `;
 
+const StyledImg = styled.img`
+  width: 100%;
+  border-radius: var(--border-radius-md);
+`;
+const StyledSearchIcon = styled(SearchIcon)`
+  opacity: 0;
+  visibility: hidden;
+  scale: 0;
+  position: absolute;
+  color: var(--color-text);
+`;
 const StyledGridItem = styled.div`
   width: 100%;
-  /* height: 100px; */
-  background-color: #ddd;
+  background-color: var(--color-background);
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 18px;
+  border-radius: var(--border-radius-md);
+  cursor: pointer;
+
+  &:hover {
+    ${StyledImg} {
+      opacity: 0.3;
+    }
+    ${StyledSearchIcon} {
+      opacity: 1;
+      visibility: visible;
+      scale: 1.4;
+      transition: all 0.4s;
+    }
+  }
 `;
+const images = ["bank1.jpg", "money.jpg"];
 
 export default function Gallery() {
-  const images = ["bank1.jpg", "money.jpg"];
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (img) => {
+    setOpen(true);
+    setSelectedImage(img);
+  };
+  const handleClose = () => setOpen(false);
 
   return (
     <StyledGaleryContainer>
+      <Heading head="Gallery" />
       <StyledGridContainer>
         {images.map((img, index) => (
-          <StyledGridItem key={index}>
-            <img
+          <StyledGridItem key={index} onClick={() => handleOpen(img)}>
+            <StyledImg
               src={`../../../public/img/${img}`}
               alt={`Resim ${index + 1}`}
             />
+            <StyledSearchIcon />
           </StyledGridItem>
         ))}
       </StyledGridContainer>
+      <Modal handleClose={handleClose} open={open} image={selectedImage} />
     </StyledGaleryContainer>
   );
 }
