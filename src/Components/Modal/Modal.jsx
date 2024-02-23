7e0873c31e640cc4b53b27f79bf6c7b519512cc1
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-02-23 22:58:35 +0300

    fixed: add slider

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -2,6 +2,7 @@
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import styled from "styled-components";
+import Slider from "../Slider/Slider";
 
 const StyledBox = styled(Box)`
   position: absolute;
@@ -30,8 +31,51 @@ export default function GalleryModal({ handleClose, open, image }) {
       aria-describedby="modal-modal-description"
     >
       <StyledBox>
+        <Slider />
         <StyledImg src={`../../../public/img/${image}`} />
       </StyledBox>
     </Modal>
   );
 }
+
+//////////////////////////////////////ilk verison
+
+// /* eslint-disable react/prop-types */
+// import Box from "@mui/material/Box";
+// import Modal from "@mui/material/Modal";
+// import styled from "styled-components";
+// import Slider from "../Slider/Slider";
+
+// const StyledBox = styled(Box)`
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   border: none;
+//   width: 60%;
+//   height: 85dvh;
+
+//   &:focus-visible {
+//     outline: none;
+//   }
+// `;
+// const StyledImg = styled.img`
+//   width: 100%;
+//   height: 100%;
+//   border: none;
+// `;
+// export default function GalleryModal({ handleClose, open, image }) {
+//   return (
+//     <Modal
+//       open={open}
+//       onClose={handleClose}
+//       aria-labelledby="modal-modal-title"
+//       aria-describedby="modal-modal-description"
+//     >
+//       <StyledBox>
+
+//         <StyledImg src={`../../../public/img/${image}`} />
+//       </StyledBox>
+//     </Modal>
+//   );
+// }
