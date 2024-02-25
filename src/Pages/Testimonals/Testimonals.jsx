0f8d1c4e29f8c6d4667ba6e75cd2306d4db526d9
Kadir <kadirht@hotmail.com>
2024-02-25 17:53:12 +0300

    fix: path fix

--- src/Pages/Testimonals/Testimonals.jsx
+++ src/Pages/Testimonals/Testimonals.jsx
@@ -12,17 +12,17 @@ const StyledContainer = styled.div`
 const data = [
     {
         desc: "Discover premium financial solutions. Managing your accounts, saving, and investing has never been this easy. Get one step closer to your financial goals with a secure, fast, and user-friendly experience.",
-        avatar: "../../../public/img/user1.jpg",
+        avatar: "../../../img/user1.jpg",
         name: "Alice",
     },
     {
         desc: "Explore cutting-edge financial solutions tailored just for you. From efficient account management to smart saving and strategic investing, we're here to empower your financial journey. Unlock a seamless, secure, and user-friendly experience.",
-        avatar: "../../../public/img/user2.jpg",
+        avatar: "../../../img/user2.jpg",
         name: "Belma",
     },
     {
         desc: "Open the door to smart saving with our range of Savings Accounts. Secure your financial future with flexible options designed to suit your goals. Enjoy competitive interest rates and user-friendly features, making your savings journey effortless and rewarding.",
-        avatar: "../../../public/img/user3.jpg",
+        avatar: "../../../img/user3.jpg",
         name: "Han Lue",
     },
 ];
