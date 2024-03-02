04287a498e6d3a4d1db4fd55a7d523cbc82a21b3
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-03 01:08:45 +0300

    style:fix styling

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -8,54 +8,50 @@ import Grow from "@mui/material/Grow";
 import PropTypes from "prop-types";
 
 CustomAccordion.propTypes = {
-    index: PropTypes.number.isRequired,
-    expanded: PropTypes.bool.isRequired,
-    handleChange: PropTypes.func.isRequired,
-    question: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  expanded: PropTypes.bool.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  question: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
 };
 
 function CustomAccordion({ index, expanded, handleChange, question, content }) {
-    return (
-        <Accordion
-            sx={{
-                width: "100%",
-                minHeight: "3.5rem",
-                backgroundColor: "var(--color-background)",
-                pt: "0.5rem",
-            }}
-            expanded={expanded === index}
-            onChange={handleChange(index)}
+  return (
+    <Accordion
+      sx={{
+        width: "100%",
+        minHeight: "3.5rem",
+        backgroundColor: "transparent",
+        pt: "0.5rem",
+      }}
+      expanded={expanded === index}
+      onChange={handleChange(index)}
+    >
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon sx={{ color: "var(--color-text)" }} />}
+        aria-controls={`${index}-content`}
+        id={`${index}-header`}
+      >
+        <Typography
+          sx={{
+            width: "maxContent",
+            flexShrink: 0,
+            fontWeight: "bold",
+            color: "var(--color-text)",
+          }}
         >
-            <AccordionSummary
-                expandIcon={
-                    <ExpandMoreIcon sx={{ color: "var(--color-text)" }} />
-                }
-                aria-controls={`${index}-content`}
-                id={`${index}-header`}
-            >
-                <Typography
-                    sx={{
-                        width: "maxContent",
-                        flexShrink: 0,
-                        fontWeight: "bold",
-                        color: "var(--color-text)",
-                    }}
-                >
-                    {question}
-                </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Grow in={expanded} timeout={500}>
-                    <Typography
-                        sx={{ color: "var(--color-text)", opacity: 0.7 }}
-                    >
-                        {content}
-                    </Typography>
-                </Grow>
-            </AccordionDetails>
-        </Accordion>
-    );
+          {question}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grow in={expanded} timeout={500}>
+          <Typography sx={{ color: "var(--color-text)", opacity: 0.7 }}>
+            {content}
+          </Typography>
+        </Grow>
+      </AccordionDetails>
+    </Accordion>
+  );
 }
 
 export default CustomAccordion;
