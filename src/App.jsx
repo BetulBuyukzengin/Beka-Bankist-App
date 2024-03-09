964e034742757c0d1300971647aa5e8fc753bb0a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-09 23:44:29 +0300

    feat: router new version updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
 import { DarkModeProvider } from "./Contexts/DarkModeContext.jsx";
 import GlobalStyles from "./styles/GlobalStyles";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+} from "react-router-dom";
 import Login from "./Pages/App/Login/Login.jsx";
 import Presentational from "./Pages/Presentational/Presentational.jsx";
 import CreateAccount from "./Pages/App/CreateAccount/CreateAccount.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Presentational,
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/createaccount",
+    Component: CreateAccount,
+  },
+]);
+
 function App() {
   return (
     <DarkModeProvider>
       <GlobalStyles />
-      <BrowserRouter>
+
+      <RouterProvider router={router} />
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Presentational />} />
           <Route path="/login" element={<Login />} />
           <Route path="/createaccount" Component={CreateAccount} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </DarkModeProvider>
   );
 }
