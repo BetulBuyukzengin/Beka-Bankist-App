45493f9e7b2f4add3413a2a2f31400bfb17ca571
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-04-20 00:39:05 +0300

    feat:create modal for receipt

---
+++ src/Pages/App/Movements/ReceiptContent.jsx
@@ -0,0 +1,114 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import styled from "styled-components";
+
+const StyledLabel = styled.label`
+  width: 50%;
+`;
+const StyledBox = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 55%;
+  background-color: var(--color-background-2);
+  color: var(--color-text);
+  padding: 2rem 1rem;
+  border-radius: 5px;
+`;
+const StyledDescription = styled.p`
+  font-size: 14px;
+`;
+export default function ReceiptContent() {
+  return (
+    <StyledBox sx={{ flexGrow: 1 }}>
+      <Box mb={4}>
+        <h5>BEKA BANKİST</h5>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            marginBottom: "2rem",
+            border: "1px solid var(--color-text)",
+            padding: "1rem",
+            borderRadius: "5px",
+            marginLeft: "0!important",
+          }}
+        >
+          <Grid
+            item
+            xs={12}
+            sx={{
+              paddingLeft: "0!important",
+              display: "flex",
+            }}
+          >
+            <StyledLabel>ŞUBE KODU/ADI: </StyledLabel>
+            <StyledLabel>Meram/Konya</StyledLabel>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              paddingLeft: "0!important",
+              display: "flex",
+            }}
+          >
+            <StyledLabel>IBAN: </StyledLabel>
+            <StyledLabel>TR15415534535315</StyledLabel>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              paddingLeft: "0!important",
+              display: "flex",
+            }}
+          >
+            <StyledLabel>İŞLEM TARİHİ: </StyledLabel>
+            <StyledLabel>14/04/2024-11.22.12</StyledLabel>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            marginBottom: "2rem",
+            marginLeft: "2rem",
+            border: "1px solid var(--color-text)",
+            borderRadius: "5px",
+          }}
+        >
+          <Grid item xs={12}>
+            SAYIN BETÜL BZ
+          </Grid>
+          <Grid item xs={12} sx={{ paddingTop: "0!important" }}>
+            adres adres adres adsersf sdklflskf adres adres adres adsersf
+            sdklflskf
+          </Grid>
+        </Grid>
+      </Box>
+      <Grid container>
+        <Grid item xs={6}>
+          <StyledDescription>
+            Withdrawn 60TL from your accouunt
+          </StyledDescription>
+          <StyledDescription>
+            fatura oluşturulma tarihi 15/04/2024-15.30.05
+          </StyledDescription>
+        </Grid>
+        <Grid item xs={6} sx={{ paddingLeft: "3%" }}>
+          <StyledDescription>
+            REGARDS BEKA BANKIST ETHERNET BRANCH
+          </StyledDescription>
+        </Grid>
+      </Grid>
+    </StyledBox>
+  );
+}
--- src/Pages/App/Movements/TableHeads/DetailTableHead.jsx
+++ src/Pages/App/Movements/TableHeads/DetailTableHead.jsx
@@ -1,7 +1,19 @@
 import { Receipt } from "@mui/icons-material";
 import { TableCell, TableHead, TableRow } from "@mui/material";
+import styled from "styled-components";
+import Modal from "@mui/material/Modal";
+import ReceiptContent from "../ReceiptContent";
+import { useState } from "react";
+
+const StyledButton = styled.button`
+  background-color: transparent;
+  border: none;
+`;
 
 function DetailTableHead() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <TableHead>
       <TableRow>
@@ -13,10 +25,18 @@ function DetailTableHead() {
         <TableCell sx={{ color: "var(--color-text)" }}>Iban</TableCell>
         <TableCell sx={{ color: "var(--color-text)" }}>Branch</TableCell>
         <TableCell sx={{ color: "var(--color-text)", borderBottom: "none" }}>
-          {/* <IconButton> */}
-          <Receipt />
-          {/* </IconButtonsssss> */}
+          <StyledButton onClick={handleOpen}>
+            <Receipt />
+          </StyledButton>
         </TableCell>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <ReceiptContent />
+        </Modal>
       </TableRow>
     </TableHead>
   );
