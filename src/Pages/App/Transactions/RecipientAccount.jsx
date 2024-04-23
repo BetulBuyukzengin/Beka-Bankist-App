f4385e6b186a98660241da337927b1ea4c6b540a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-04-24 01:37:46 +0300

    feat:add recipient account page

--- src/Pages/App/Transactions/RecipientAccount.jsx
+++ src/Pages/App/Transactions/RecipientAccount.jsx
@@ -1,5 +1,19 @@
+import CustomTabs from "./Tabs/CustomTabs";
+import SavedRecipientTab from "./Tabs/RegisteredRecipientTab";
+import NewRecipientTab from "./Tabs/NewRecipientTab";
+
+const content = [
+  {
+    label: "Saved Recipients",
+    component: <SavedRecipientTab />,
+  },
+  {
+    label: "New Recipient",
+    component: <NewRecipientTab />,
+  },
+];
 function RecipientAccount() {
-  return <div>recipient account</div>;
+  return <CustomTabs content={content} />;
 }
 
 export default RecipientAccount;
