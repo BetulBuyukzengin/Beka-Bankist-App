0ef3fb753abc7c6dede00b381d7b2aa75331c5f6
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-04-24 23:42:14 +0300

    refactor: refactor transfer money tab

--- src/Pages/App/Transactions/Tabs/TransferMoneyTab.jsx
+++ src/Pages/App/Transactions/Tabs/TransferMoneyTab.jsx
@@ -125,7 +125,13 @@ export default function TransferMoneyTab() {
               )}
             </>
           ))}
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "end",
+            }}
+          >
             <Button
               color="inherit"
               disabled={activeStep === 0}
@@ -134,12 +140,12 @@ export default function TransferMoneyTab() {
             >
               Back
             </Button>
-            <Box sx={{ flex: "1 1 auto" }} />
-            {isStepOptional(activeStep) && (
+            {/* <Box sx={{ flex: "1 1 auto" }} /> */}
+            {/* {isStepOptional(activeStep) && (
               <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
                 Skip
               </Button>
-            )}
+            )} */}
 
             <Button onClick={handleNext}>
               {activeStep === steps.length - 1 ? "Finish" : "Next"}
