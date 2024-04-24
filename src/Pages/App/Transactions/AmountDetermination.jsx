fe9bbcf2af73b26b0c65e8b6fcd532d776495b3d
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-04-25 02:08:50 +0300

    feat: create amount determination

--- src/Pages/App/Transactions/AmountDetermination.jsx
+++ src/Pages/App/Transactions/AmountDetermination.jsx
@@ -1,5 +1,67 @@
+import { Box, Grid, TextField } from "@mui/material";
+import { formatCurrency } from "../../../utils/utils";
+const GridStyle = {
+  display: "flex",
+  justifyContent: "center",
+  backgroundColor: "transparent",
+  color: "var(--color-text)",
+  width: "100%",
+  // padding: "1.5rem 2rem",
+};
+const BoxStyle = {
+  width: "40%",
+  border: "1px solid var(--color-border-2)",
+  padding: "1.5rem 3rem",
+  justifyContent: "space-between",
+  display: "flex",
+  borderRadius: "5px",
+};
 function AmountDetermination() {
-  return <div>AmountDetermination</div>;
+  return (
+    <Grid container spacing={2}>
+      <Grid
+        item
+        xs={12}
+        // component="button"
+        sx={GridStyle}
+      >
+        <Box sx={BoxStyle}>
+          <label>Kalan para transfer limiti:</label>
+          <label>{formatCurrency(1000000000)}</label>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        // component="button"
+        sx={GridStyle}
+      >
+        <Box sx={BoxStyle}>
+          <label>Sık gönderilen miktar:</label>
+          <label>{formatCurrency(500)}</label>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sx={GridStyle}
+        // component="button"
+      >
+        <Box sx={{ ...BoxStyle, padding: "0", border: "none" }}>
+          <TextField
+            id="outlined-basic"
+            type="number"
+            label="Gönderilecek miktar"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default AmountDetermination;
