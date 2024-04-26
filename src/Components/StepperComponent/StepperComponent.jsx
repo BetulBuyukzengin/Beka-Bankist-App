8c0991221d36ae1b17ecb3e5343154c663878292
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-04-26 21:07:06 +0300

    feat:create reuseable stepper component

---
+++ src/Components/StepperComponent/StepperComponent.jsx
@@ -0,0 +1,89 @@
+/* eslint-disable react/prop-types */
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function StepperComponent({ transactionSteps }) {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={activeStep}>
+        {transactionSteps.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+          return (
+            <Step key={index} {...stepProps}>
+              <StepLabel {...labelProps}></StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      <React.Fragment>
+        {transactionSteps.map((transaction, index) => (
+          <>
+            {index === activeStep && (
+              <Box
+                sx={{
+                  height: "22rem",
+                  overflowY: "auto",
+                }}
+              >
+                <Typography key={index} sx={{ mt: 2, mb: 1 }}>
+                  {transaction.label}
+                </Typography>
+                <Box>{transaction.component}</Box>
+              </Box>
+            )}
+          </>
+        ))}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "end",
+          }}
+        >
+          <Button
+            color="inherit"
+            disabled={activeStep === 0}
+            onClick={handleBack}
+            sx={{ mr: 1 }}
+          >
+            Back
+          </Button>
+          <Button onClick={handleNext}>
+            {activeStep === transactionSteps.length - 1 ? "Finish" : "Next"}
+          </Button>
+        </Box>
+      </React.Fragment>
+    </Box>
+  );
+}
