8b8763e0abdeeb59c7600776ded4755763e8a124
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-04-26 21:10:43 +0300

    feat:add stepperComponent in transfer money tab

--- src/Pages/App/Transactions/Tabs/TransferMoneyTab.jsx
+++ src/Pages/App/Transactions/Tabs/TransferMoneyTab.jsx
@@ -1,134 +1,27 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import RecipientAccount from "../RecipientAccount";
 import DetailDetermination from "../DetailDetermination";
 import AmountDetermination from "../AmountDetermination";
 import SenderAccount from "../SenderAccount";
+import StepperComponent from "../../../../Components/StepperComponent/StepperComponent";
 
-const steps = [
-  "Select campaign settings",
-  "Create an ad group",
-  "Create an ad",
-  "Create an ad",
+const transactionSteps = [
+  {
+    label: "Recipient Account",
+    component: <RecipientAccount />,
+  },
+  {
+    label: "Sender Account",
+    component: <SenderAccount />,
+  },
+  {
+    label: "Amount Determination",
+    component: <AmountDetermination />,
+  },
+  {
+    label: "Detail Determination",
+    component: <DetailDetermination />,
+  },
 ];
-
 export default function TransferMoneyTab() {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  // const handleSkip = () => {
-  //   if (!isStepOptional(activeStep)) {
-  //     throw new Error("You can't skip a step that isn't optional.");
-  //   }
-
-  //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  //   setSkipped((prevSkipped) => {
-  //     const newSkipped = new Set(prevSkipped.values());
-  //     newSkipped.add(activeStep);
-  //     return newSkipped;
-  //   });
-  // };
-
-  const transactionSteps = [
-    {
-      label: "Recipient Account",
-      component: <RecipientAccount />,
-    },
-    {
-      label: "Sender Account",
-      component: <SenderAccount />,
-    },
-    {
-      label: "Amount Determination",
-      component: <AmountDetermination />,
-    },
-    {
-      label: "Detail Determination",
-      component: <DetailDetermination />,
-    },
-  ];
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={index} {...stepProps}>
-              <StepLabel {...labelProps}></StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      <React.Fragment>
-        {transactionSteps.map((transaction, index) => (
-          <>
-            {index === activeStep && (
-              <Box
-                sx={{
-                  height: "22rem",
-                  overflowY: "auto",
-                }}
-              >
-                <Typography key={index} sx={{ mt: 2, mb: 1 }}>
-                  {transaction.label}
-                </Typography>
-                <Box>{transaction.component}</Box>
-              </Box>
-            )}
-          </>
-        ))}
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "end",
-          }}
-        >
-          <Button
-            color="inherit"
-            disabled={activeStep === 0}
-            onClick={handleBack}
-            sx={{ mr: 1 }}
-          >
-            Back
-          </Button>
-          <Button onClick={handleNext}>
-            {activeStep === steps.length - 1 ? "Finish" : "Next"}
-          </Button>
-        </Box>
-      </React.Fragment>
-    </Box>
-  );
+  return <StepperComponent transactionSteps={transactionSteps} />;
 }
