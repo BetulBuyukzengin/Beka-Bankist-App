fe31858ea641ecd531a77f5edf422ba695185a66
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-04-26 21:09:31 +0300

    feat:add stepper component

--- src/Pages/App/Transactions/Tabs/LoanTab.jsx
+++ src/Pages/App/Transactions/Tabs/LoanTab.jsx
@@ -1,5 +1,27 @@
+import StepperComponent from "../../../../Components/StepperComponent/StepperComponent";
+import BusinessInformation from "../Loan/BusinessInformation";
+import PersonalInformation from "../Loan/PersonalInformation";
+
+const transactionSteps = [
+  {
+    label: "Personal Information",
+    component: <PersonalInformation />,
+  },
+  {
+    label: "Business Information",
+    component: <BusinessInformation />,
+  },
+  // {
+  //   label: "Amount Determination",
+  //   component: <AmountDetermination />,
+  // },
+  // {
+  //   label: "Detail Determination",
+  //   component: <DetailDetermination />,
+  // },
+];
 function LoanTab() {
-  return <div>Loan</div>;
+  return <StepperComponent transactionSteps={transactionSteps} />;
 }
 
 export default LoanTab;
