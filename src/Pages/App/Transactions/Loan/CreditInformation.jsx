82a22955f502bcdf4adcd3a0908456aa29d7153a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-04-30 00:20:41 +0300

    feat: add select box and handle functions for change values

--- src/Pages/App/Transactions/Loan/CreditInformation.jsx
+++ src/Pages/App/Transactions/Loan/CreditInformation.jsx
@@ -1,5 +1,10 @@
-import { Grid, TextField } from "@mui/material";
+import { Grid, MenuItem, Select, TextField } from "@mui/material";
+import { useState } from "react";
 import styled from "styled-components";
+import {
+  calculateAndFormatLoan,
+  formatCurrency,
+} from "../../../../utils/utils";
 
 const StyledTextField = styled(TextField)`
   width: 40%;
@@ -18,6 +23,16 @@ const StyledTextField = styled(TextField)`
 `;
 
 export default function CreditInformation() {
+  const [paymentPlan, setPaymentPlan] = useState(1);
+  const [loanAmount, setLoanAmount] = useState();
+
+  const handlePlanChange = (event) => {
+    setPaymentPlan(event.target.value);
+  };
+
+  function handleLoanAmountChange(e) {
+    setLoanAmount(e.target.value);
+  }
   return (
     <Grid
       xs={12}
@@ -25,13 +40,14 @@ export default function CreditInformation() {
       spacing={3}
       sx={{
         display: "flex",
-        // border: "1px solid var(--color-border-2)",
         paddingBottom: "1rem",
         paddingRight: "1rem",
       }}
     >
       <Grid item xs={6}>
         <StyledTextField
+          value={loanAmount}
+          onChange={handleLoanAmountChange}
           id="outlined-basic"
           label="Loan Amount"
           fullWidth
@@ -54,16 +70,58 @@ export default function CreditInformation() {
         />
       </Grid>
       <Grid item xs={6}>
-        <StyledTextField
-          type="text"
-          id="outlined-basic"
-          label="Kredi ödeme planı"
-          variant="outlined"
-          fullWidth
-          InputLabelProps={{
-            shrink: true,
+        <Select
+          sx={{
+            "&&": {
+              width: "40%",
+              backgroundColor: "transparent",
+              color: "var(--color-text)",
+              "&:hover > fieldset ": {
+                borderColor: "var(--color-text) !important",
+              },
+            },
+            "&>fieldset": {
+              borderColor: "var(--color-text)",
+            },
           }}
-        />
+          id="demo-customized-select-native"
+          value={paymentPlan}
+          onChange={handlePlanChange}
+          fullWidth
+          displayEmpty
+          MenuProps={{
+            anchorOrigin: {
+              horizontal: "center",
+              vertical: "bottom",
+            },
+            transformOrigin: {
+              vertical: "top",
+              horizontal: "center",
+            },
+            getContentAnchorEl: null,
+            PaperProps: {
+              style: {
+                maxHeight: 200,
+                width: 20,
+                left: "200px",
+                horizontal: "center",
+                backgroundColor: "var(--color-background-2)",
+                color: "var(--color-text)",
+              },
+            },
+          }}
+        >
+          <MenuItem value="">Loan payment plan</MenuItem>
+          <MenuItem value={1}>
+            {calculateAndFormatLoan(loanAmount, 0.02, 1 * 12)}
+          </MenuItem>
+          <MenuItem value={2}>
+            {calculateAndFormatLoan(loanAmount, 0.02, 2 * 12)}
+          </MenuItem>
+          <MenuItem value={3}>
+            {calculateAndFormatLoan(loanAmount, 0.02, 3 * 12)}
+          </MenuItem>
+        </Select>
       </Grid>
     </Grid>
   );
