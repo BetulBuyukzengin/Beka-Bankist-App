bd3bc176c6ff3f67a92954a751db8eafb9b56116
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-01 23:20:38 +0300

    feat:create custom select

---
+++ src/Components/CustomSelect/CustomSelect.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/prop-types */
+import { MenuItem, Select } from "@mui/material";
+
+function CustomSelect({ value, handleChange, data, width, disabled }) {
+  return (
+    <Select
+      sx={{
+        "&&": {
+          color: "var(--color-text)",
+          backgroundColor: "transparent",
+          opacity: disabled ? "0.6!important" : "1!important",
+          width: width ? "40%!important" : "100%!important",
+          "&> .Mui-disabled": {
+            WebkitTextFillColor: "var(--color-text)",
+          },
+          "&:hover > fieldset ": {
+            borderColor: disabled
+              ? "var(--color-border-2)!important"
+              : "var(--color-gray)!important",
+          },
+          "&>fieldset": {
+            borderColor: "var(--color-border-2)",
+          },
+        },
+      }}
+      id="demo-customized-select-native"
+      value={value}
+      onChange={handleChange}
+      fullWidth
+      displayEmpty
+      disabled={disabled}
+      MenuProps={{
+        anchorOrigin: {
+          horizontal: "center",
+          vertical: "bottom",
+        },
+        transformOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+        getContentAnchorEl: null,
+        PaperProps: {
+          style: {
+            maxHeight: 200,
+            width: 20,
+            left: "200px",
+            horizontal: "center",
+            backgroundColor: "var(--color-background-2)",
+            color: "var(--color-text)",
+            "& > .MuiList-root ": {
+              padding: "5rem!important",
+              backgroundColor: "red!important",
+            },
+          },
+        },
+      }}
+    >
+      {data.map((item, index) => (
+        <MenuItem key={index} value={item.value}>
+          {item.content}
+        </MenuItem>
+      ))}
+    </Select>
+  );
+}
+
+export default CustomSelect;
