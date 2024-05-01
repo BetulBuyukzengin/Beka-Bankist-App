46cf705e34ff64a8fe31c0282a5bae1455bfbe81
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-01 23:20:51 +0300

    feat:create custom text field

---
+++ src/Components/CustomTextField/CustomTextField.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import { TextField } from "@mui/material";
+import styled from "styled-components";
+
+const StyledTextField = styled(TextField)`
+  width: ${(props) =>
+    props.width === "short" ? "40%!important" : "100%!important"};
+
+  &:hover > div > fieldset {
+    border-color: var(--color-gray) !important;
+  }
+  & > label {
+    color: var(--color-text);
+  }
+  & > div {
+    color: var(--color-text);
+    & > fieldset {
+      border-color: var(--color-border-2);
+    }
+  }
+`;
+
+function CustomTextField({
+  label,
+  id,
+  value,
+  width,
+  type = "text",
+  defaultValue,
+  onChange,
+}) {
+  return (
+    <StyledTextField
+      onChange={onChange}
+      defaultValue={defaultValue}
+      width={width}
+      value={value}
+      id={id}
+      type={type}
+      label={label}
+      fullWidth
+      variant="outlined"
+      InputLabelProps={{
+        shrink: true,
+      }}
+    />
+  );
+}
+
+export default CustomTextField;
