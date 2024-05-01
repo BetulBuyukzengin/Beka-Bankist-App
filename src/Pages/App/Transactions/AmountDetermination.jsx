2cfa269875d1f0ee0185f436b2ea5d4ce0011b2a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-01 23:23:17 +0300

    refactor: refactor amount determination

--- src/Pages/App/Transactions/AmountDetermination.jsx
+++ src/Pages/App/Transactions/AmountDetermination.jsx
@@ -1,83 +1,59 @@
-import { Box, Grid, TextField } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { formatCurrency } from "../../../utils/utils";
+
+import CustomTextField from "../../../Components/CustomTextField/CustomTextField";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
 const GridStyle = {
   display: "flex",
   justifyContent: "center",
   backgroundColor: "transparent",
   color: "var(--color-text)",
-  width: "100%",
-  // padding: "1.5rem 2rem",
 };
 const BoxStyle = {
-  width: "40%",
-  border: "1px solid var(--color-text)",
+  border: "1px solid var(--color-border-2)",
+  borderRadius: "5px",
   padding: "1.5rem 3rem",
+  width: "40%",
   justifyContent: "space-between",
   display: "flex",
-  borderRadius: "5px",
 };
 
+const frequentlyAmount = Math.trunc(Math.random() * 1000000);
+
 function AmountDetermination() {
+  const [showFrequentlyAmount, setShowFrequentlyAmount] = useState();
+  function handleClick() {
+    setShowFrequentlyAmount(frequentlyAmount);
+  }
+  function handleChange(e) {
+    setShowFrequentlyAmount(e.target.value);
+  }
   return (
     <Grid container spacing={2}>
-      <Grid
-        item
-        xs={12}
-        // component="button"
-        sx={GridStyle}
-      >
+      <Grid item xs={12} sx={GridStyle}>
         <Box sx={BoxStyle}>
-          <label>Kalan para transfer limiti:</label>
+          <label>Remaining transfer limit:</label>
           <label>{formatCurrency(1000000000)}</label>
         </Box>
       </Grid>
 
-      <Grid
-        item
-        xs={12}
-        // component="button"
-        sx={GridStyle}
-      >
-        <Box sx={BoxStyle}>
-          <label>Sık gönderilen miktar:</label>
-          <label>{formatCurrency(500)}</label>
+      <Grid item xs={12} sx={GridStyle}>
+        <Box sx={{ ...BoxStyle, cursor: "pointer" }} onClick={handleClick}>
+          <label style={{ cursor: "pointer" }}>Frequently sent amount:</label>
+          <label style={{ cursor: "pointer" }}>
+            {formatCurrency(frequentlyAmount)}
+          </label>
         </Box>
       </Grid>
 
-      <Grid
-        item
-        xs={12}
-        sx={GridStyle}
-        // component="button"
-      >
+      <Grid item xs={12} sx={GridStyle}>
         <Box sx={{ ...BoxStyle, padding: "0", border: "none" }}>
-          <TextField
-            sx={{
-              width: "40%",
-              "&:hover > div > fieldset": {
-                borderColor: "var(--color-text)!important",
-              },
-              "&>label": {
-                color: "var(--color-text)",
-              },
-              "&>div": {
-                // backgroundColor: "pink",
-                color: "var(--color-text)",
-                "&>fieldset": {
-                  borderColor: "var(--color-text)",
-                },
-              },
-            }}
-            id="outlined-basic"
-            type="text"
-            label="Gönderilecek miktar"
-            variant="outlined"
-            fullWidth
-            InputLabelProps={{
-              shrink: true,
-            }}
+          <CustomTextField
+            id="amountToSent"
+            width="tall"
+            label="Amount to sent"
+            value={showFrequentlyAmount || ""}
+            onChange={(e) => handleChange(e)}
           />
         </Box>
       </Grid>
