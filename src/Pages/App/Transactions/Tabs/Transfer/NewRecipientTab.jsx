79b53fa028bc32408aa41517e0139096ef743f8c
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-02 16:36:31 +0300

    feat:create new recipient tab

---
+++ src/Pages/App/Transactions/Tabs/Transfer/NewRecipientTab.jsx
@@ -0,0 +1,22 @@
+import CustomTabs from "../../../../../Components/CustomTabs/CustomTabs";
+import WithIbanTab from "./WithIbanTab";
+import AccountNumberTab from "./AccountNumberTab";
+const content = [
+  {
+    label: "With Iban",
+    component: <WithIbanTab />,
+  },
+  { label: "With Account Numbers", component: <AccountNumberTab /> },
+];
+// can make with use qr code
+
+function NewRecipientTab() {
+  const orientation = "vertical";
+  return (
+    <>
+      <CustomTabs orientation={orientation} content={content} />
+    </>
+  );
+}
+
+export default NewRecipientTab;
