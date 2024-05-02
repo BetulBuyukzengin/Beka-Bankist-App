17c37f1c508fd3bed56321d0f4965c0197f8a443
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-02 16:34:20 +0300

    feat: create financial information

---
+++ src/Pages/App/Transactions/Tabs/Loan/FinancialInformation.jsx
@@ -0,0 +1,27 @@
+import { Grid } from "@mui/material";
+import CustomTextField from "../../../../../Components/CustomTextField/CustomTextField";
+
+export default function FinancialInformation() {
+  return (
+    <Grid
+      xs={12}
+      container
+      spacing={3}
+      sx={{
+        display: "flex",
+        paddingBottom: "1rem",
+        paddingRight: "1rem",
+      }}
+    >
+      <Grid item xs={6}>
+        <CustomTextField id="totalIncome" label="Total Income" />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomTextField type="text" id="totalExpense" label="Total Expense" />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomTextField type="text" id="-basic" label="Current Debts" />
+      </Grid>
+    </Grid>
+  );
+}
