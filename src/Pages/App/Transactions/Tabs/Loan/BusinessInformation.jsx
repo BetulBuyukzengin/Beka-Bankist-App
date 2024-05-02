06ef8b4b3ff732c4c2c4ab5c42a78a158b3f903d
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-02 16:33:39 +0300

    feat: create businessInformation

---
+++ src/Pages/App/Transactions/Tabs/Loan/BusinessInformation.jsx
@@ -0,0 +1,30 @@
+import { Grid } from "@mui/material";
+import CustomTextField from "../../../../../Components/CustomTextField/CustomTextField";
+
+export default function BusinessInformation() {
+  return (
+    <Grid
+      xs={12}
+      container
+      spacing={3}
+      sx={{
+        display: "flex",
+        paddingBottom: "1rem",
+        paddingRight: "1rem",
+      }}
+    >
+      <Grid item xs={6}>
+        <CustomTextField id="businessName" label="Business Name" />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomTextField id="sectorofWork" label="Sector of Work" />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomTextField id="job" label="Job" />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomTextField type="number" id="income" label="Income" />
+      </Grid>
+    </Grid>
+  );
+}
