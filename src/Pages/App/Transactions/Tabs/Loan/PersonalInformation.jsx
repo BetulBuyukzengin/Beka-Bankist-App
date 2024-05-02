29aeeeb1b9af7040708caaaaa2ff4352067cac3e
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-02 16:35:16 +0300

    feat: create personal information

---
+++ src/Pages/App/Transactions/Tabs/Loan/PersonalInformation.jsx
@@ -0,0 +1,45 @@
+import { Grid } from "@mui/material";
+import { useState } from "react";
+import CustomTextField from "../../../../../Components/CustomTextField/CustomTextField";
+import CustomDatePicker from "../../../../../Components/CustomDatePicker/CustomDatePicker";
+
+export default function PersonalInformation() {
+  const [value, setValue] = useState(new Date());
+
+  return (
+    <Grid
+      xs={12}
+      container
+      spacing={3}
+      sx={{
+        display: "flex",
+        paddingBottom: "1rem",
+        paddingRight: "1rem",
+      }}
+    >
+      <Grid item xs={6}>
+        <CustomTextField id="fullName" label="Full Name" />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomTextField
+          id="identificationNumber"
+          label="Identification number"
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomTextField id="adress" label="Adress" />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomTextField id="phoneNumber" label="Phone number" />
+      </Grid>
+      <Grid item xs={6}>
+        <CustomDatePicker
+          width="tall"
+          label="Birthday"
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        />
+      </Grid>
+    </Grid>
+  );
+}
