2c8a68c8c65608a5ded04296a9dd3fa6a4d6508a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-05 01:24:37 +0300

    feat: add custom radio component

--- src/Pages/App/Transactions/Tabs/Transfer/SenderAccount.jsx
+++ src/Pages/App/Transactions/Tabs/Transfer/SenderAccount.jsx
@@ -1,26 +1,9 @@
 /* eslint-disable react/prop-types */
-import { NavigateNext } from "@mui/icons-material";
-import { Box, FormControlLabel, Grid, Radio, RadioGroup } from "@mui/material";
-import React from "react";
+import { Box, Grid } from "@mui/material";
 import { useFormContext } from "react-hook-form";
 import styled from "styled-components";
+import CustomRadio from "../../../../../Components/CustomRadio/CustomRadio";
 
-const StyledH6 = styled.h6`
-  text-align: start;
-`;
-const StyledTitleLabel = styled.label`
-  margin-right: 0.5rem;
-`;
-const StyledValueLabel = styled.label``;
-
-const StyledAccountCheckComponent = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 7% 9%;
-  border: 1px solid var(--color-border-2);
-`;
 const StyledBox = styled(Box)`
   background-color: transparent;
   display: flex;
@@ -29,29 +12,9 @@ const StyledBox = styled(Box)`
   width: 25%;
   cursor: pointer;
 `;
-function AccountCheckComp() {
-  return (
-    <StyledAccountCheckComponent>
-      <div>
-        <StyledH6>Meram-500</StyledH6>
-        <StyledTitleLabel>Kullanılabilir bakiye:</StyledTitleLabel>
-        <StyledValueLabel>500</StyledValueLabel>
-      </div>
-      <div>
-        <NavigateNext />
-      </div>
-    </StyledAccountCheckComponent>
-  );
-}
 
 function SenderAccount() {
   const { register } = useFormContext();
-
-  const [value, setValue] = React.useState("female");
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
 
   return (
     <Grid container spacing={2}>
@@ -64,34 +27,7 @@ function SenderAccount() {
         }}
       >
         <StyledBox>
-          <RadioGroup
-            aria-labelledby="demo-controlled-radio-buttons-group"
-            name="controlled-radio-buttons-group"
-            value={value}
-            onChange={handleChange}
-          >
-            <FormControlLabel
-              sx={{ marginLeft: "0", marginRight: "0" }}
-              control={
-                <Radio
-                  {...register("accountCheckCompRadio")}
-                  sx={{
-                    // opacity: "0",
-                    height: "20%",
-                    width: "21.5%",
-                    position: "absolute",
-                    borderRadius: "0px",
-                    "&+span": {
-                      width: "17rem",
-                    },
-                  }}
-                />
-              }
-              value="AccountCheckComp"
-              {...register("accountCheckComp")}
-              label={<AccountCheckComp />}
-            />
-          </RadioGroup>
+          <CustomRadio register={{ ...register("selectedAccount") }} />
         </StyledBox>
       </Grid>
     </Grid>
