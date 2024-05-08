6cea8a82b5c197b987484b901f0d5051919a86e4
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-09 01:54:33 +0300

    feat: create registered recipients tab

---
+++ src/Pages/App/Transactions/Tabs/Transfer/RegisteredRecipientsTab.jsx
@@ -0,0 +1,47 @@
+import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
+import { useFormContext } from "react-hook-form";
+
+const GridStyle = {
+  justifyContent: "center",
+  alignItems: "center",
+  display: "flex",
+  cursor: "pointer",
+  "&:first-of-type": {
+    marginTop: "1rem",
+  },
+};
+const BoxStyle = {
+  border: "1px solid var(--color-border-2)",
+  borderRadius: "5px",
+  padding: "1rem 3rem",
+  width: "40%",
+  justifyContent: "center",
+  display: "flex",
+};
+function RegisteredRecipientsTab() {
+  const { getValues } = useFormContext();
+  const { saveAsRegisteredWithAccount, saveAsRegisteredWithIban, shortName } =
+    getValues();
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        backgroundColor: "transparent",
+        color: "var(--color-text)",
+      }}
+    >
+      <Grid item xs={6} sx={GridStyle}>
+        <Box sx={BoxStyle}>
+          <label style={{ cursor: "pointer" }}>{shortName}</label>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default RegisteredRecipientsTab;
