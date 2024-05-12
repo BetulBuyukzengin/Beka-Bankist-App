c57736b9fe7c6a5b65d4d61898172b83c20e3540
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-12 22:20:13 +0300

    feat:add create movements func

--- src/services/movementsServices.js
+++ src/services/movementsServices.js
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "../Supabase/supabase";
+import { toast } from "react-toastify";
 
 export async function getMovements() {
   let { data: movements, error } = await supabase.from("movements").select("*");
@@ -12,4 +13,28 @@ export function useMovements() {
     queryKey: ["movements"],
   });
   return { movements, isLoading };
+}
+
+export async function createMovements(newMovement) {
+  const { data, error } = await supabase
+    .from("movements")
+    .insert([newMovement]);
+  if (error) throw new Error(error.message);
+  return data;
+}
+
+export function useCreateMovements() {
+  const queryClient = useQueryClient();
+  const { mutateAsync: createMovement, isLoading: isCreating } = useMutation({
+    mutationFn: (data) => createMovements(data),
+    onSuccess: () => {
+      toast.success("Transfer was successfully completed");
+      queryClient.invalidateQueries({ queryKey: ["movements"] });
+    },
+    onError: () =>
+      toast.error(
+        "An error occurred during transfer transaction, please try again later!"
+      ),
+  });
+  return { isCreating, createMovement };
 }
