245893e5796a3e097ee6934930fac2724e3b33e0
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-15 02:10:11 +0300

    feat: create account

---
+++ src/Pages/App/Account/Account.jsx
@@ -0,0 +1,15 @@
+import Loader from "../../../Components/Loader/Loader";
+import { useGetAccounts } from "../../../services/accountServices";
+import BankAccountCreate from "./BankAccountCreate";
+import CurrentAccounts from "./CurrentAccounts";
+
+function Account() {
+  const { accounts, isLoading } = useGetAccounts();
+  //   console.log(accounts);
+  if (isLoading) return <Loader />;
+  return (
+    <>{accounts.length > 0 ? <CurrentAccounts /> : <BankAccountCreate />}</>
+  );
+}
+
+export default Account;
