31220698e9878d50447c3723cd0aefabd38af93e
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-15 02:08:03 +0300

    feat: create calc age, generate random iban, generate random bank account number

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 import { format } from "date-fns";
+import { differenceInDays } from "date-fns";
 
 export const formatCurrency = (value) =>
   new Intl.NumberFormat("tr", { style: "currency", currency: "TRY" }).format(
@@ -33,4 +34,59 @@ export function generatePaymentMethod(method) {
   if (method === "housingRent") return "Housing Rent";
   if (method === "workplaceRent") return "Workplace Rent";
   if (method === "eCommercePayment") return "E-Commerce Payment";
+}
+
+export function calcAge(birthday) {
+  return differenceInDays(new Date(), birthday) / 365;
+}
+
+export function generateRandomIBAN() {
+  const bankCodes = ["TR"];
+  const countryCode = bankCodes[Math.floor(Math.random() * bankCodes.length)];
+
+  const trBankCodes = [
+    "001",
+    "002",
+    "003",
+    "004",
+    "005",
+    "006",
+    "007",
+    "008",
+    "009",
+    "010",
+  ];
+  const bankCode = trBankCodes[Math.floor(Math.random() * trBankCodes.length)];
+
+  const accountNumber = Math.floor(
+    1000000000000000 + Math.random() * 9000000000000000
+  );
+
+  const iban = countryCode + bankCode + accountNumber;
+
+  return iban;
+}
+
+export function generateRandomBankAccountNumber() {
+  const bankCodes = [
+    "001",
+    "002",
+    "003",
+    "004",
+    "005",
+    "006",
+    "007",
+    "008",
+    "009",
+    "010",
+  ];
+  const bankCode = bankCodes[Math.floor(Math.random() * bankCodes.length)];
+
+  // Rastgele 10 haneli rakam üretme
+  const accountNumber = Math.floor(1000000000 + Math.random() * 9000000000);
+
+  // Banka hesap numarasını formatlama
+  const formattedAccountNumber = ("0000000000" + accountNumber).slice(-10); // 10 haneli olacak şekilde formatla
+
+  return bankCode + formattedAccountNumber;
 }
