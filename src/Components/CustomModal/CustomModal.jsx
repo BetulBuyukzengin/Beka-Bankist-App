f1abf2b78de2876ceb025c748bb4b7795557f873
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-15 23:09:31 +0300

    feat: create custom modal

---
+++ src/Components/CustomModal/CustomModal.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/prop-types */
+import { Box, Modal, Typography } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: "3rem 2rem",
+
+  scrollY: "auto",
+  maxHeight: "30rem",
+  maxWidth: "70rem",
+  overflowY: "auto",
+
+  "&:focus-visible": {
+    outline: "none",
+  },
+};
+
+function CustomModal({ children, setOpen, open, title }) {
+  return (
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography
+          variant="h4"
+          sx={{ marginBottom: "1rem", textAlign: "center" }}
+        >
+          {title}
+        </Typography>
+        {children}
+      </Box>
+    </Modal>
+  );
+}
+
+export default CustomModal;
