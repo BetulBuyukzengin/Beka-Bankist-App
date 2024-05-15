72aea0c311efc77e86a33d22921844e7415963e4
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-15 23:11:27 +0300

    feat: add form provider for account consent form

--- src/Pages/App/Account/AccountCreate.jsx
+++ src/Pages/App/Account/AccountCreate.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Box, Checkbox, Grid, Modal, Typography } from "@mui/material";
 import CustomButton from "../../../Components/CustomButton/CustomButton";
 import CustomSelect from "../../../Components/CustomSelect/CustomSelect";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import {
   calcAge,
   generateRandomBankAccountNumber,
@@ -18,6 +18,8 @@ import {
   useGetAccounts,
 } from "../../../services/accountServices";
 import Loader from "../../../Components/Loader/Loader";
+import CustomModal from "../../../Components/CustomModal/CustomModal";
+import AccountConsentForm from "./AccountConsentForm";
 
 const bankContent = [
   {
@@ -62,26 +64,14 @@ const StyledMuiTelInput = styled(MuiTelInput)`
 `;
 const StyledContainer = styled.div``;
 const StyledLabel = styled.label``;
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-};
 
 function AccountCreate() {
-  const { register, handleSubmit, setValue } = useForm();
+  const methods = useForm();
+  // const { register, handleSubmit, setValue } = useForm();
   const [birthday, setBirthday] = useState(new Date());
   const [phoneNumber, setPhoneNumber] = useState();
   const [open, setOpen] = useState(false);
   const { isLoading, mutateAsync } = useCreateAccount();
-  const { accounts } = useGetAccounts();
-
- 
 
   function handleChangePhone(phone) {
     setPhoneNumber(phone);
@@ -91,108 +81,101 @@ function AccountCreate() {
     //hesaplar tablosundan ibanları, hesap no kıyasla
     const accountNumber = generateRandomBankAccountNumber();
     const formDatas = { ...data, phoneNumber, iban, accountNumber, balance: 0 };
-    // console.log(formDatas);
     await mutateAsync(formDatas);
   }
   if (isLoading) return <Loader />;
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid
-          container
-          spacing={3}
-          sx={{
-            display: "flex",
-            paddingBottom: "1rem",
-            paddingRight: "1rem",
-          }}
-        >
-          <Grid item xs={6}>
-            <CustomTextField
-              id="fullName"
-              label="Full Name"
-              register={register("fullName")}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <CustomTextField
-              id="address"
-              label="Address"
-              register={register("address")}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <CustomSelect
-              data={bankContent}
-              defaultValue=""
-              register={register("bankName")}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <CustomSelect
-              data={branchContent}
-              defaultValue=""
-              register={register("bankBranch")}
-            />
-          </Grid>
-
-          <Grid item xs={6}>
-            <CustomDatePicker
-              width="tall"
-              label="Birthday"
-              value={birthday}
-              onChange={(date) => {
-                setBirthday(date);
-                setValue("birthday", date);
-                if (calcAge(date) < 18) {
-                  toast.error("You are younger than 18 ");
-                }
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <StyledMuiTelInput
-              preferredCountries={["TR", "US", "KR"]}
-              defaultCountry="TR"
-              value={phoneNumber}
-              onChange={(phone) => handleChangePhone(phone)}
-            />
-          </Grid>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
           <Grid
-            item
-            xs={12}
+            container
+            spacing={3}
             sx={{
               display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-              alignItems: "center",
+              paddingBottom: "1rem",
+              paddingRight: "1rem",
             }}
           >
-            <StyledContainer>
-              <Checkbox />
-              <StyledLabel onClick={() => setOpen(true)}>
-                ASRŞLSKSAFŞSKMFŞ
-              </StyledLabel>
-            </StyledContainer>
-            <CustomButton type="submit" buttonText="create" />
+            <Grid item xs={6}>
+              <CustomTextField
+                id="fullName"
+                label="Full Name"
+                register={methods.register("fullName")}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CustomTextField
+                id="address"
+                label="Address"
+                register={methods.register("address")}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CustomSelect
+                data={bankContent}
+                defaultValue=""
+                register={methods.register("bankName")}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CustomSelect
+                data={branchContent}
+                defaultValue=""
+                register={methods.register("bankBranch")}
+              />
+            </Grid>
+
+            <Grid item xs={6}>
+              <CustomDatePicker
+                width="tall"
+                label="Birthday"
+                value={birthday}
+                onChange={(date) => {
+                  setBirthday(date);
+                  methods.setValue("birthday", date);
+                  if (calcAge(date) < 18) {
+                    toast.error("You are younger than 18 ");
+                  }
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <StyledMuiTelInput
+                preferredCountries={["TR", "US", "KR"]}
+                defaultCountry="TR"
+                value={phoneNumber}
+                onChange={(phone) => handleChangePhone(phone)}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <StyledContainer>
+                <Checkbox />
+                <StyledLabel onClick={() => setOpen(true)}>
+                  ASRŞLSKSAFŞSKMFŞ
+                </StyledLabel>
+              </StyledContainer>
+              <CustomButton type="submit" buttonText="create" />
+            </Grid>
           </Grid>
-        </Grid>
-      </form>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+          <CustomModal
+            open={open}
+            setOpen={setOpen}
+            title="Bank Account Opening Disclosure and Approval Form"
+          >
+            <AccountConsentForm phoneNumber={phoneNumber} />
+          </CustomModal>
+        </form>
+      </FormProvider>
     </>
   );
 }
