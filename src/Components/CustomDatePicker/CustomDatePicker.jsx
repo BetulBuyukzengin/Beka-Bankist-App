a794279d8e3341f658afc10effad5a1504681d61
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-18 02:20:20 +0300

    refactor: refactor

--- src/Components/CustomDatePicker/CustomDatePicker.jsx
+++ src/Components/CustomDatePicker/CustomDatePicker.jsx
@@ -1,13 +1,34 @@
 import { DatePicker } from "@mui/x-date-pickers";
 
 /* eslint-disable react/prop-types */
-function CustomDatePicker({ value, onChange, label, width, register, margin }) {
+function CustomDatePicker({
+  value,
+  onChange,
+  label,
+  width,
+  register,
+  margin,
+  // required,
+  helperText,
+  error,
+}) {
+  const isError = value;
   return (
     <DatePicker
       label={label}
       value={value}
+      // error={error}
+      // required={required}
       onChange={onChange}
-      slotProps={{ popper: { placement: "right-start" } }}
+      slotProps={{
+        popper: { placement: "right-start" },
+        textField: {
+          // required: "This field is required!",
+          helperText: helperText,
+          // required: required ? "This field is required!" : undefined,
+          error: isError,
+        },
+      }}
       {...register}
       sx={{
         marginTop: margin === "small" && "1rem",
@@ -22,7 +43,10 @@ function CustomDatePicker({ value, onChange, label, width, register, margin }) {
           color: "var(--color-text)",
 
           "& > fieldset": {
-            borderColor: "var(--color-border-2)!important",
+            borderColor: isError
+              ? "red !important"
+              : "var(--color-border-2) !important",
+            // borderColor: "var(--color-border-2)!important",
           },
         },
       }}
