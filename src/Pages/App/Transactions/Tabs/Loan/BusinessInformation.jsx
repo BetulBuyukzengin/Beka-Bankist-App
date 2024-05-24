3bbc2222970ab8f309453dbf76b749c9dd3ba98a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-24 22:42:26 +0300

    feat: add register and required

--- src/Pages/App/Transactions/Tabs/Loan/BusinessInformation.jsx
+++ src/Pages/App/Transactions/Tabs/Loan/BusinessInformation.jsx
@@ -1,7 +1,11 @@
 import { Grid } from "@mui/material";
 import CustomTextField from "../../../../../Components/CustomTextField/CustomTextField";
+import { useFormContext } from "react-hook-form";
 
 export default function BusinessInformation() {
+  const { register, formState } = useFormContext();
+  const { errors } = formState;
+  //! register ve required ekledim ama 2.stepe gcınce hata veriyor
   return (
     <Grid
       container
@@ -13,16 +17,58 @@ export default function BusinessInformation() {
       }}
     >
       <Grid item xs={6}>
-        <CustomTextField id="businessName" label="Business Name" />
+        <CustomTextField
+          id="businessName"
+          label="Business Name"
+          register={{
+            ...register("applicantBusinessName", {
+              required: "Th,s field is required!",
+            }),
+          }}
+          helperText={errors?.applicantBusinessName?.message}
+          error={errors?.applicantBusinessName}
+        />
       </Grid>
       <Grid item xs={6}>
-        <CustomTextField id="sectorofWork" label="Sector of Work" />
+        <CustomTextField
+          id="sectorofWork"
+          label="Sector of Work"
+          register={{
+            ...register("applicantSectorOfWork", {
+              required: "This field is required!",
+            }),
+          }}
+          helperText={errors?.applicantSectorOfWork?.message}
+          error={errors?.applicantSectorOfWork}
+        />
       </Grid>
       <Grid item xs={6}>
-        <CustomTextField id="job" label="Job" />
+        <CustomTextField
+          id="job"
+          label="Job"
+          register={{
+            ...register("applicantJob", {
+              required: "This field is required!",
+            }),
+          }}
+          helperText={errors?.applicantJob?.message}
+          error={errors?.applicantJob}
+        />
       </Grid>
+
       <Grid item xs={6}>
-        <CustomTextField type="number" id="income" label="Income" />
+        <CustomTextField
+          type="number"
+          id="income"
+          label="Income"
+          register={{
+            ...register("applicantIncome", {
+              required: "This field is required!",
+            }),
+          }}
+          helperText={errors?.applicantIncome?.message}
+          error={errors?.applicantIncome}
+        />
       </Grid>
     </Grid>
   );
