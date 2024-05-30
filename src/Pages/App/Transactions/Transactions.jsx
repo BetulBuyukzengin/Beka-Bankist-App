6cd61c351dd851cc435f771a1e719d1ffbd654cc
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-30 23:13:14 +0300

    feat: add use effect for tab change

--- src/Pages/App/Transactions/Transactions.jsx
+++ src/Pages/App/Transactions/Transactions.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import CustomTabs from "../../../Components/CustomTabs/CustomTabs";
 import DepositTab from "./Tabs/Deposit/DepositTab";
 import LoanTab from "./Tabs/Loan/LoanTab";
 import TransferMoneyTab from "./Tabs/Transfer/TransferMoneyTab";
 import WithdrawTab from "./Tabs/Withdraw/WithdrawTab";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 const content = [
   {
@@ -24,17 +24,39 @@ const content = [
     component: <WithdrawTab />,
   },
 ];
+const preserveParams = ["transactions-tab", "recipient-account-tab", "status"];
 function Transactions() {
-  const { pathname } = useLocation();
+  const { search, pathname } = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
+  const prevParamValueRef = useRef(null);
+  const navigate = useNavigate();
+  const currentParamValue = searchParams.get("transactions-tab");
 
+  //! Important
+  useEffect(
+    function () {
+      const handleClearParams = () => {
+        const params = new URLSearchParams(search);
+        const newSearchParams = new URLSearchParams();
+        preserveParams.forEach((param) => {
+          if (params.has(param)) newSearchParams.set(param, params.get(param));
+        });
+        const newUrl = `${pathname}?${newSearchParams.toString()}`; // Sadece path'i al, search kısmını temizle
+
+        navigate(newUrl, { replace: true }); // replace: true, history stack'ini değiştirmeden yönlendirme yapar
+      };
+      if (currentParamValue !== prevParamValueRef.current) {
+        handleClearParams();
+      }
+      prevParamValueRef.current = currentParamValue;
+    },
+    [navigate, search, searchParams, currentParamValue, pathname]
+  );
   useEffect(function () {
-    if (pathname === "/applayout/transactions") {
-      searchParams.set("transactions-tab", 0);
-      searchParams.set("recipient-account-tab", 0);
-      searchParams.set("status", content[0].label);
-      setSearchParams(searchParams);
-    }
+    searchParams.set("transactions-tab", 0);
+    searchParams.set("recipient-account-tab", 0);
+    searchParams.set("status", content[0].label);
+    setSearchParams(searchParams);
   }, []);
 
   return (
