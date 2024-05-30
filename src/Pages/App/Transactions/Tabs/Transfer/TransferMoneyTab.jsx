39f15d4504c3546eaf71dc67359ae0a70037003a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-05-30 23:15:42 +0300

    feat: add yup for validation

--- src/Pages/App/Transactions/Tabs/Transfer/TransferMoneyTab.jsx
+++ src/Pages/App/Transactions/Tabs/Transfer/TransferMoneyTab.jsx
@@ -12,7 +12,12 @@ import { useUser } from "../../../../../services/userServices";
 import { useUpdateBalance } from "../../../../../services/accountServices";
 import { useSearchParams } from "react-router-dom";
 import Loader from "../../../../../Components/Loader/Loader";
-
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  maxIbanLength,
+  minAmountToSend,
+} from "../../../../../Constants/constants";
 const transactionSteps = [
   {
     label: "Recipient Account",
@@ -41,13 +46,76 @@ export default function TransferMoneyTab() {
   const { createMovement, isCreating } = useCreateMovements();
   // const { isLoading, mutateAsync } = useCreateAccount();
   const { user } = useUser();
-  const methods = useForm();
+
   const senderFullName = user.user_metadata.fullName;
   const [searchParams] = useSearchParams();
   const selectedAccount = JSON.parse(searchParams.get("selectedAccount"));
   const id = selectedAccount?.id;
   const { isLoading, mutateAsync: updateBalance } = useUpdateBalance();
+  const validationSchema = [
+    //validation for step 1 iban
 
+    yup.object({
+      recipientIban: yup
+        .string()
+        .required("This field is required!")
+        .test(
+          "is-valid-iban",
+          "Iban is invalid, should be 26 characters long!",
+          (value) => value.length === maxIbanLength
+        ),
+      // prettier-ignore
+      recipientFullNameWithIban: yup.string().required("This field is required!"),
+      // shortName: yup.string().required("This field is required!"),
+      // saveAsRegisteredWithIban: yup
+      //   .boolean()
+      //   .required("This field is required!"),
+    }),
+
+    //validation for step 1 account number tab
+    // yup.object({
+    //   recipientBankName: yup.string().required("This field is required!"),
+    //   recipientBankBranch: yup.string().required("This field is required!"),
+    //   recipientAccountNumber: yup.string().required("This field is required!"),
+    //   recipientFullNameWithAccount: yup
+    //     .string()
+    //     .required("This field is required!"),
+    //   shortName: yup.string().required("This field is required!"),
+    //   saveAsRegisteredWithAccount: yup
+    //     .string()
+    //     .required("This field is required!"),
+    // }),
+
+    //validation for step 3 sender account
+    yup.object({
+      selectedAccount: yup.string().required("This field is required!"),
+    }),
+    //validation for step 4 amount determinationda kaldım
+    yup.object({
+      amountToSend: yup
+        .number()
+        .min(
+          minAmountToSend,
+          `Send amount should be grater than ${minAmountToSend}!`
+        )
+        .required("This field is required!"),
+    }),
+    yup.object({
+      paymentMethod: yup.string().required("This field is required!"),
+      transferDescription: yup.string().required("This field is required!"),
+      showUsernameDescription: yup.boolean(),
+    }),
+    yup.object({
+      transactionDate: yup.string().required("This field is required!"),
+    }),
+    yup.object({}),
+  ];
+  const currentValidationSchema = validationSchema[activeStep];
+
+  const methods = useForm({
+    resolver: yupResolver(currentValidationSchema),
+    mode: "onChange",
+  });
   const onSubmit = async (data) => {
     const updatedBalance = selectedAccount.balance - +data.amountToSend;
     await createMovement({ ...data, senderFullName });
