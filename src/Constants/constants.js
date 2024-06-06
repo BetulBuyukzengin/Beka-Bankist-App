b06ec3976e0a462baa783009f54aef8ff47ed6d2
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-06-06 23:13:07 +0300

    feat: add identification number digit

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -2,3 +2,4 @@ export const minAmountToSend = 10;
 export const starterBalance = 0;
 export const dailyTransferLimit = 500000;
 export const maxIbanLength = 32; // with spaces
+export const identificationNumberDigit = 11;
