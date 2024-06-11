79ff809c6d4119643bd685e4a589ad55ebdfebb5
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-06-11 22:36:53 +0300

    feat: create dailyDepositLimit and dailyWithdrawLimit variables

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,5 +1,7 @@
 export const minAmountToSend = 10;
 export const starterBalance = 0;
 export const dailyTransferLimit = 500000;
+export const dailyDepositLimit = 50000;
+export const dailyWithdrawLimit = 50000;
 export const maxIbanLength = 32; // with spaces
 export const identificationNumberDigit = 11;
