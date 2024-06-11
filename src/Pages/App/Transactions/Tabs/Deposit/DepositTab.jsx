693792afd043c3c33470a491bc88b8b0a25e7053
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-06-11 22:42:33 +0300

    feat: create deposit tab component and data operations with supabase

--- src/Pages/App/Transactions/Tabs/Deposit/DepositTab.jsx
+++ src/Pages/App/Transactions/Tabs/Deposit/DepositTab.jsx
@@ -1,5 +1,117 @@
+import { FormProvider, useForm } from "react-hook-form";
+import Deposit from "./Deposit";
+import StepperComponent from "../../../../../Components/StepperComponent/StepperComponent";
+import MyAccounts from "./MyAccounts";
+import React from "react";
+import { useUpdateBalance } from "../../../../../services/accountServices";
+import Loader from "../../../../../Components/Loader/Loader";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  calcRemainingLimitResetTime,
+  formatCurrency,
+  showDailyLimitMessage,
+} from "../../../../../utils/utils";
+import { useCreateMovements } from "../../../../../services/movementsServices";
+
+const transactionSteps = [
+  {
+    label: "My Accounts",
+    component: <MyAccounts />,
+  },
+  {
+    label: "Deposit",
+    component: <Deposit />,
+  },
+];
+
 function DepositTab() {
-  return <div>deposit tab</div>;
+  const { isLoading, mutateAsync: depositMoney } = useUpdateBalance();
+  const navigate = useNavigate();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [searchParams] = useSearchParams();
+  const selectedAccount = JSON.parse(searchParams.get("selectedAccount"));
+  const id = selectedAccount?.id;
+  const currentBalance = +selectedAccount?.balance;
+  const currentDepositLimit = +selectedAccount?.remainingDepositLimit;
+  const { createMovement, isCreating } = useCreateMovements();
+  const validationSchema = [
+    yup.object({
+      selectedAccount: yup.string().required("This field is required!"),
+    }),
+    yup.object({
+      amountToBeDepositMyAccount: yup
+        .string()
+        .test("max-check", function (value) {
+          console.log(+value);
+          if (+value > currentDepositLimit && 0 !== currentDepositLimit) {
+            return this.createError({
+              message: `Daily deposit limit is ${formatCurrency(
+                currentDepositLimit
+              )}`,
+            });
+          }
+          if (0 === currentDepositLimit) {
+            return this.createError({
+              message: showDailyLimitMessage(
+                "deposit",
+                calcRemainingLimitResetTime()
+              ),
+            });
+          } else return true;
+        })
+        .required("This field is required!"),
+    }),
+    yup.object({}),
+  ];
+  const currentValidationSchema = validationSchema[activeStep];
+
+  const methods = useForm({
+    resolver: yupResolver(currentValidationSchema),
+    mode: "onChange",
+  });
+
+  const getStatus = searchParams.get("status");
+
+  const onSubmit = async (data) => {
+    const { amountToBeDepositMyAccount } = data;
+    const updatedAccount = {
+      ...selectedAccount,
+      remainingDepositLimit:
+        currentDepositLimit - Number(amountToBeDepositMyAccount),
+      balance: currentBalance + Number(amountToBeDepositMyAccount),
+    };
+    console.log(updatedAccount);
+    await depositMoney({ id, account: updatedAccount });
+    await createMovement({
+      selectedAccount: updatedAccount,
+      status: getStatus,
+      amountToBeDepositMyAccount,
+    });
+    navigate("/applayout/account");
+  };
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <StepperComponent
+          transactionSteps={transactionSteps}
+          activeStep={activeStep}
+          setActiveStep={setActiveStep}
+        />
+        {activeStep === transactionSteps.length && (
+          <div style={{ margin: "3rem", textAlign: "center" }}>
+            {isLoading
+              ? "Transfer transaction is in progress"
+              : "Transfer was successfully completed ..."}
+          </div>
+        )}
+      </form>
+    </FormProvider>
+  );
 }
 
 export default DepositTab;
