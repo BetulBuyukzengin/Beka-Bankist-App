f33e63948ee9d3699dbe211f9d893e25b7b232d5
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-07-29 15:26:59 +0300

    feat: add const variables

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -4,4 +4,9 @@ export const dailyTransferLimit = 500000;
 export const dailyDepositLimit = 50000;
 export const dailyWithdrawLimit = 50000;
 export const maxIbanLength = 32; // with spaces
-export const identificationNumberDigit = 11;
+export const maxAccountNumberLength = 16; // with spaces
+export const transferPrice = 2; // For week days
+
+export const identificationNumberCharacter = 11;
+export const interestAmountConst = 5;
+export const hourInterval = 10800000; // every 3 hours
