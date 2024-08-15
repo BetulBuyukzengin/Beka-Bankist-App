a71376c6c64ce925799600834861d5d1688e71bd
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-16 00:07:58 +0300

    fixed: delete unnecessary code

--- src/Pages/App/Settings/UpdateEmail/UpdateEmailAdressForm.jsx
+++ src/Pages/App/Settings/UpdateEmail/UpdateEmailAdressForm.jsx
@@ -1,24 +1,34 @@
+/* eslint-disable react/prop-types */
 import CustomTabs from "../../../../Components/CustomTabs/CustomTabs";
 import UpdateEmailWithEmail from "./UpdateEmailTab/UpdateEmailWithEmail";
 import UpdateEmailWithPhoneNumber from "./UpdateEmailTab/UpdateEmailWithPhoneNumber";
 
-const content = [
-  {
-    label: "With Email",
-    component: <UpdateEmailWithEmail />,
-  },
-  { label: "With Phone Number", component: <UpdateEmailWithPhoneNumber /> },
-];
+// const content = [
+//   {
+//     label: "With Email",
+//     component: <UpdateEmailWithEmail />,
+//   },
+//   { label: "With Phone Number", component: <UpdateEmailWithPhoneNumber /> },
+// ];
 
-function UpdateEmailAdressForm() {
-  const mainTabLabel = content.map((tab) => {
-    return tab.label;
-  });
+function UpdateEmailAdressForm({ setOpenUpdateEmailModal }) {
+  // const mainTabLabel = content.map((tab) => {
+  //   return tab.label;
+  // });
   return (
     <CustomTabs
       tabName="UpdateEmailAdressForm"
-      mainTabLabel={mainTabLabel}
-      content={content}
+      // mainTabLabel={mainTabLabel}
+      content={[
+        {
+          label: "With Email",
+          component: <UpdateEmailWithEmail setOpen={setOpenUpdateEmailModal} />,
+        },
+        {
+          label: "With Phone Number",
+          component: <UpdateEmailWithPhoneNumber />,
+        },
+      ]}
     />
   );
 }
