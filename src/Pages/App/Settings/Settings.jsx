3ff06c91bbf94b15d0e98473c52684c55150118a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-16 00:16:36 +0300

    feat: add personal information settings

--- src/Pages/App/Settings/Settings.jsx
+++ src/Pages/App/Settings/Settings.jsx
@@ -2,6 +2,7 @@ import { Grid } from "@mui/material";
 import UpdateEmail from "./UpdateEmail/UpdateEmail";
 import UpdatePassword from "./UpdatePassword/UpdatePassword";
 import DeleteAppAccount from "./DeleteAppAccount/DeleteAppAccount";
+import PersonalInformationSettings from "./PersonalInformationSettings";
 function Settings() {
   return (
     <Grid
@@ -9,6 +10,9 @@ function Settings() {
       gap={2}
       sx={{ justifyContent: "center", textAlign: "center" }}
     >
+      <Grid item xs={6}>
+        <PersonalInformationSettings />
+      </Grid>
       <Grid item xs={6}>
         <UpdateEmail />
       </Grid>
