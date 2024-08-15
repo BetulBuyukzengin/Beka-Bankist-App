244b63709932069a79eba4a76d81b8e1823a7a4d
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-16 00:10:07 +0300

    feat: implement use form &  update email

--- src/Pages/App/Settings/UpdateEmail/UpdateEmailTab/UpdateEmailWithEmail.jsx
+++ src/Pages/App/Settings/UpdateEmail/UpdateEmailTab/UpdateEmailWithEmail.jsx
@@ -1,11 +1,73 @@
+/* eslint-disable react/prop-types */
 import { Grid } from "@mui/material";
 import { useForm } from "react-hook-form";
-import CustomTextField from "../../../../../Components/CustomTextField/CustomTextField";
+import { toast } from "react-toastify";
 import CustomButton from "../../../../../Components/CustomButton/CustomButton";
-function UpdateEmailWithEmail() {
-  const { register } = useForm();
+import CustomTextField from "../../../../../Components/CustomTextField/CustomTextField";
+import { useUpdateUser, useUser } from "../../../../../services/userServices";
+import { supabase } from "../../../../../Supabase/supabase";
+import { useEffect, useState } from "react";
+import { useCurrentUserContext } from "../../../../../Contexts/CurrentUserContext";
+
+function UpdateEmailWithEmail({ setOpen }) {
+  const { register, handleSubmit } = useForm();
+  // Get the authenticated user and the user from the users table whose id is the same
+  const { currentUser } = useCurrentUserContext();
+
+  const { user } = useUser();
+  const { mutateAsync: updateUser } = useUpdateUser();
+
+  //? Doesn't working cuz of dates are not updated correctly
+  // const sentAt = new Date(user.email_change_sent_at);
+  // const updatedAt = new Date(user.updated_at);
+  // const isSentAtBeforeUpdatedAt = isBefore(sentAt, updatedAt);
+
+  // useEffect(() => {
+  //   async function updateUsersTableUser() {
+  //     await updateUser({
+  //       id: currentUser.id,
+  //       user: { ...currentUser, email: user.email },
+  //     });
+
+  // await supabase.auth.updateUser({
+  //   data: { email_verified: true },
+  // });
+  //? user.user_metadata.email_verified not updating by supabase
+  //   }
+  // if (user.user_metadata.email_verified && user.new_email) {
+  //     console.log("Email confirmed. Proceeding with the operation...");
+  //     updateUsersTableUser();
+  //   }
+  //   // }
+  // }, [user.user_metadata.email_verified, user.new_email, currentUser]);
+
+  const onSubmit = async (data) => {
+    try {
+      // Update email in authenticated
+      await supabase.auth.updateUser(
+        {
+          email: data.newEmailAddress,
+          // data: { email_verified: false },
+        }
+        // { emailRedirectTo: data.newEmailAddress }
+      );
+
+      toast.success("Please check your email!");
+
+      //! Burada mail onaylanmadan direkt güncelleniyor...
+      await updateUser({
+        id: currentUser.id,
+        user: { ...currentUser, email: data.newEmailAddress },
+      });
+
+      setOpen(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Grid
         container
         gap={2}
@@ -29,9 +91,12 @@ function UpdateEmailWithEmail() {
         </Grid>
         <Grid item xs={6}>
           <CustomTextField
+            textTransform="basic"
             id="yourEmail"
             // type="text"
             label="Your Email Address"
+            defaultValue={user.email}
+            disabled
             register={{
               ...register("yourEmailAddress"),
             }}
@@ -39,6 +104,7 @@ function UpdateEmailWithEmail() {
         </Grid>
         <Grid item xs={6}>
           <CustomTextField
+            textTransform="basic"
             id="newEmail"
             type="text"
             label="New Email Address"
@@ -49,6 +115,7 @@ function UpdateEmailWithEmail() {
         </Grid>
         <Grid item xs={6}>
           <CustomTextField
+            textTransform="basic"
             id="password"
             // type="text"
             label="Password"
@@ -58,7 +125,10 @@ function UpdateEmailWithEmail() {
           />
         </Grid>
         <Grid item xs={6}>
-          <CustomButton buttonText="Send verification email" />
+          <CustomButton
+            type={handleSubmit}
+            buttonText="Send verification email"
+          />
         </Grid>
       </Grid>
     </form>
