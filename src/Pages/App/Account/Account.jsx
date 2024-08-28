c16f803917218ee7d8d27fa7f662c44f8af0e153
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-28 16:39:31 +0300

    feat: add redirect to settings based on is informations completed

--- src/Pages/App/Account/Account.jsx
+++ src/Pages/App/Account/Account.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Loader from "../../../Components/Loader/Loader";
+import { useIsUserInformation } from "../../../Hooks/useIsUserInformation";
 import { useGetAccounts } from "../../../services/accountServices";
 import BankAccountCreate from "./BankAccountCreate";
 import CurrentAccounts from "./CurrentAccounts";
 
 function Account() {
   const { accounts, isLoading } = useGetAccounts();
+  // const { currentUser } = useCurrentUser();
+  const navigate = useNavigate();
+  const { isInformationsCompleted } = useIsUserInformation();
+  useEffect(() => {
+    if (!isInformationsCompleted) {
+      navigate("/applayout/settings", { replace: true });
+    }
+  }, []);
 
   if (isLoading) return <Loader />;
   return (
