c94050f3465276709fdbec326bd85630a677b3f2
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-28 17:02:57 +0300

    feat: pass prop

--- src/Pages/App/Transactions/Tabs/Deposit/Deposit.jsx
+++ src/Pages/App/Transactions/Tabs/Deposit/Deposit.jsx
@@ -1,21 +1,27 @@
 import { Grid } from "@mui/material";
 import CustomTextField from "../../../../../Components/CustomTextField/CustomTextField";
 import { useFormContext } from "react-hook-form";
+import { useSearchParams } from "react-router-dom";
 
 function Deposit() {
   const {
     register,
     formState: { errors },
   } = useFormContext();
-
+  const [searchParams] = useSearchParams();
+  const selectedAccount = JSON.parse(searchParams.get("selectedAccount"));
+  console.log(selectedAccount);
   return (
     <Grid container sx={{ padding: "1rem" }}>
       <Grid item xs={12}>
         <CustomTextField
+          width="short"
           id="depositMoney"
           label="Amount to be deposit"
           type="number"
-          register={{ ...register("amountToBeDepositMyAccount") }}
+          register={{
+            ...register("amountToBeDepositMyAccount"),
+          }}
           helperText={errors?.amountToBeDepositMyAccount?.message}
           error={errors?.amountToBeDepositMyAccount}
         />
