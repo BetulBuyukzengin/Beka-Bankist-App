f9cfae2b2c3d946a7a1e7b401bd9119641608e18
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-28 17:26:18 +0300

    feat:add toast message and conditionally return true or false from the verifyuserpassword hook

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,24 +2,30 @@ import { useMutation } from "@tanstack/react-query";
 import { supabase } from "../Supabase/supabase";
 import { toast } from "react-toastify";
 
-async function updateUserInformation(obj) {
-  const { data, error } = await supabase.auth.updateUser(obj);
+async function updateUserInformation({ updatedUser, toastMessage }) {
+  const { data, error } = await supabase.auth.updateUser(updatedUser);
   if (error) throw new Error(error.message);
-  return data;
+  return { data, toastMessage };
 }
 export function useUpdateUserInformation() {
   const { mutateAsync, isPending } = useMutation({
     mutationFn: updateUserInformation,
-    onError: () => toast.error("Can't update user information"),
-    onSuccess: () => toast.success("User information updated successfully"),
+    onError: (returnedObject) => toast.error(returnedObject.toastMessage.error),
+    onSuccess: (returnedObject) => {
+      toast.success(returnedObject.toastMessage.success);
+    },
   });
   return { mutateAsync, isPending };
 }
+
 export async function verifyUserPassword(userEmail, userPassword) {
   const { error: signInError } = await supabase.auth.signInWithPassword({
     email: userEmail,
     password: userPassword,
   });
-  if (signInError)
+  if (signInError) {
     toast.error("Current password is incorrect:", signInError.message);
+    return false;
+  }
+  return true;
 }
