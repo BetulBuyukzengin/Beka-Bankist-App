32137dc35145a1f51175e701999f649839e0e7af
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-28 16:58:02 +0300

    feat: If your account password is correct, update your password, add validate, helper text

--- src/Pages/App/Settings/UpdatePassword/UpdatePasswordForm.jsx
+++ src/Pages/App/Settings/UpdatePassword/UpdatePasswordForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { Grid } from "@mui/material";
 import { useForm } from "react-hook-form";
 import CustomButton from "../../../../Components/CustomButton/CustomButton";
@@ -8,16 +9,29 @@ import {
 } from "../../../../services/authServices";
 import { useUser } from "../../../../services/userServices";
 
+const toastMessage = {
+  error: "Password cannot be changed!",
+  success: "Password changed successfully!",
+};
+
 function UpdatePasswordForm({ setOpen }) {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm();
   const { isPending, mutateAsync: updatePassword } = useUpdateUserInformation();
   const { user } = useUser();
   const onSubmit = async (data) => {
-    console.log(data);
+    // console.log(data);
     // current password is true or not
     await verifyUserPassword(user.email, data.currentPassword);
     // if current password is true then update password
-    await updatePassword({ password: data.newPassword });
+    await updatePassword({
+      updatedUser: { password: data.newPassword },
+      toastMessage,
+    });
     setOpen(false);
   };
 
@@ -39,41 +53,60 @@ function UpdatePasswordForm({ setOpen }) {
         </Grid>
         <Grid item xs={6}>
           <CustomTextField
-            textTransform="basic"
+            texttransform="basic"
             id="currentPassword"
             // type="text"
-            label="Old Password"
+            label="Current Password"
             register={{
-              ...register("currentPassword"),
+              ...register("currentPassword", {
+                required: "Current password is required!",
+              }),
             }}
+            helperText={errors?.currentPassword?.message}
+            error={errors?.currentPassword}
           />
         </Grid>
         <Grid item xs={6}>
           <CustomTextField
             id="newPassword"
-            textTransform="basic"
+            texttransform="basic"
             // type="text"
             label="New Password"
             register={{
-              ...register("newPassword"),
+              ...register("newPassword", {
+                required: "New password is required!",
+                pattern: {
+                  value: /^(?=.*[A-Z])(?=.*\d).+$/, // En az bir büyük harf ve bir sayı içermesi
+                  message:
+                    "Password must contain at least one uppercase letter, one number and English characters.",
+                },
+              }),
             }}
+            helperText={errors?.newPassword?.message}
+            error={errors?.newPassword}
           />
         </Grid>
         <Grid item xs={6}>
           <CustomTextField
             id="repeatNewPassword"
-            textTransform="basic"
+            texttransform="basic"
             // type="text"
             label="Repeat New Password"
             register={{
-              ...register("repeatNewPassword"),
+              ...register("repeatNewPassword", {
+                required: "Repeat password is required!",
+                validate: (value) =>
+                  getValues().newPassword === value || "Passwords do not match",
+              }),
             }}
+            helperText={errors?.repeatNewPassword?.message}
+            error={errors?.repeatNewPassword}
           />
         </Grid>
         <Grid item xs={6}>
           <CustomButton
             buttonText="Update Password"
-            type={handleSubmit}
+            type="submit"
             disabled={isPending}
           />
         </Grid>
