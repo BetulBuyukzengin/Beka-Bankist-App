1c3bd31bcc3ee639c39077c89ca7ed5cc152f088
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-28 16:45:19 +0300

    feat: add delete account hook and clear search params func

--- src/Pages/App/Account/AccountDelete.jsx
+++ src/Pages/App/Account/AccountDelete.jsx
@@ -1,28 +1,64 @@
-import { useForm, FormProvider } from "react-hook-form";
-import CustomRadio from "../../../Components/CustomRadio/CustomRadio";
+import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { useSearchParams } from "react-router-dom";
+import CustomModal from "../../../Components/CustomModal/CustomModal";
+import CustomRadio from "../../../Components/CustomRadio/CustomRadio";
+import DeleteAccountConfirmationScreen from "./DeleteAccountConfirmationScreen";
+
+// Test
+import { useDeleteAccount } from "../../../services/accountServices";
 
 function AccountDelete() {
   const methods = useForm();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [open, setOpen] = useState(false);
+  const accountToDelete = JSON.parse(searchParams.get("selectedAccount"));
+  const accountId = accountToDelete.id;
+
+  const { mutateAsync: deleteAccount, isPending } = useDeleteAccount();
+
+  const onSubmit = async () => {
+    await deleteAccount(accountId, {
+      onSuccess: () => {
+        setOpen(false);
+        clearSearchParams();
+      },
+    });
+  };
 
   function handleChange(value) {
     searchParams.set("selectedAccount", value);
     setSearchParams(searchParams);
+    setOpen(true);
   }
-  function onSubmit() {}
+  function clearSearchParams() {
+    if (searchParams.has("selectedAccount")) {
+      searchParams.delete("selectedAccount");
+      setSearchParams(searchParams);
+    }
+  }
+  if (isPending) return console.log("siliiniyor");
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <CustomRadio
-          // border={border}
-          value={searchParams.get("selectedAccount")}
-          onChange={(e) => {
-            handleChange(e.target.value);
-          }}
-          register={{ ...methods.register("selectedAccount") }}
+      <CustomRadio
+        value={searchParams.get("selectedAccount")}
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+      />
+      <CustomModal
+        open={open}
+        setOpen={setOpen}
+        title="Confirm Delete"
+        clearParamsCallBack={clearSearchParams}
+        shouldClearParamsOnClose
+      >
+        <DeleteAccountConfirmationScreen
+          setOpen={setOpen}
+          onSubmit={onSubmit}
+          clearParamsCallBack={clearSearchParams}
         />
-      </form>
+      </CustomModal>
     </FormProvider>
   );
 }
