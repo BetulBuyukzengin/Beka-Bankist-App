0db3cf6d3d9bee6b6183c06746a86918e6937d3a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-28 16:54:36 +0300

    feat: render jsx based on isinformationcompleted and show loader

--- src/Pages/App/Settings/Settings.jsx
+++ src/Pages/App/Settings/Settings.jsx
@@ -1,9 +1,15 @@
 import { Grid } from "@mui/material";
-import UpdateEmail from "./UpdateEmail/UpdateEmail";
-import UpdatePassword from "./UpdatePassword/UpdatePassword";
+import Loader from "../../../Components/Loader/Loader";
+import { useCurrentUser } from "../../../Hooks/useCurrentUser";
 import DeleteAppAccount from "./DeleteAppAccount/DeleteAppAccount";
 import PersonalInformationSettings from "./PersonalInformationSettings";
+import UpdateEmail from "./UpdateEmail/UpdateEmail";
+import UpdatePassword from "./UpdatePassword/UpdatePassword";
+import { useIsUserInformation } from "../../../Hooks/useIsUserInformation";
 function Settings() {
+  const { currentUser } = useCurrentUser();
+  const { isInformationsCompleted } = useIsUserInformation();
+  if (!currentUser) return <Loader />;
   return (
     <Grid
       container
@@ -11,17 +17,23 @@ function Settings() {
       sx={{ justifyContent: "center", textAlign: "center" }}
     >
       <Grid item xs={6}>
-        <PersonalInformationSettings />
+        <PersonalInformationSettings
+          isPersonalDatas={isInformationsCompleted}
+        />
       </Grid>
-      <Grid item xs={6}>
-        <UpdateEmail />
-      </Grid>
-      <Grid item xs={6}>
-        <UpdatePassword />
-      </Grid>
-      <Grid item xs={6}>
-        <DeleteAppAccount />
-      </Grid>
+      {isInformationsCompleted && (
+        <>
+          <Grid item xs={6}>
+            <UpdateEmail />
+          </Grid>
+          <Grid item xs={6}>
+            <UpdatePassword />
+          </Grid>
+          <Grid item xs={6}>
+            <DeleteAppAccount />
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 }
