e262161b56c697f330c1bffb0af93ce8f2ea2bb9
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-30 00:18:35 +0300

    feat: add filter and sort methods

--- src/Pages/App/Movements/MovementsTable.jsx
+++ src/Pages/App/Movements/MovementsTable.jsx
@@ -4,11 +4,53 @@ import TableContainer from "@mui/material/TableContainer";
 import Paper from "@mui/material/Paper";
 import Rows from "./Rows";
 import MainTableHead from "./TableHeads/MainTableHead";
-import { useMovements } from "../../../services/movementsServices";
+import {
+  useFilterMovements,
+  useMovements,
+} from "../../../services/movementsServices";
+import Loader from "../../../Components/Loader/Loader";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function MovementsTable() {
   const { isLoading, movements } = useMovements();
-  if (isLoading) return;
+  const [searchParams] = useSearchParams();
+  const filter = searchParams.get("filter");
+  const sort = searchParams.get("sort");
+  const { data: filteredMovements, isLoading: isLoadingFiltered } =
+    useFilterMovements({ filter });
+
+  const [movementsState, setMovementsState] = useState(movements || []);
+
+  useEffect(() => {
+    let sortedMovements = movements;
+
+    if (filter) {
+      sortedMovements = filteredMovements || [];
+    }
+
+    if (sort) {
+      sortedMovements = [...sortedMovements].sort((a, b) => {
+        if (sort === "ascending") return a.amountToSend - b.amountToSend;
+        if (sort === "descending") return b.amountToSend - a.amountToSend;
+        if (sort === "dateAscending")
+          return new Date(a.createdAt) - new Date(b.createdAt);
+        if (sort === "dateDescending")
+          return new Date(b.createdAt) - new Date(a.createdAt);
+      });
+    }
+
+    //! Check that sortedMovements is a valid array (if null or undefined)
+    if (Array.isArray(sortedMovements)) {
+      setMovementsState(sortedMovements);
+    } else {
+      setMovementsState([]);
+      console.error("sortedMovements is not iterable:", sortedMovements);
+    }
+  }, [filter, sort, movements, filteredMovements]);
+
+  if (isLoading || isLoadingFiltered) return <Loader />;
+
   return (
     <TableContainer
       component={Paper}
@@ -24,7 +66,7 @@ export default function MovementsTable() {
       >
         <MainTableHead />
         <TableBody>
-          {movements?.map((row, id) => (
+          {movementsState?.map((row, id) => (
             <Rows key={id} row={row} />
           ))}
         </TableBody>
--- src/services/movementsServices.js
+++ src/services/movementsServices.js
@@ -1,6 +1,7 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { supabase } from "../Supabase/supabase";
 import { toast } from "react-toastify";
+import { useSearchParams } from "react-router-dom";
 
 async function getMovements() {
   let { data: movements, error } = await supabase.from("movements").select("*");
@@ -38,3 +39,48 @@ export function useCreateMovements() {
   });
   return { isCreating, createMovement };
 }
+
+//! columnToSort,directionObj ekledım burda kaldım
+async function filterMovements({
+  filter,
+  columnToSort = "amountToSend",
+  directionObj = { ascending: true },
+}) {
+  let { data, error } = await supabase
+    .from("movements")
+    .select("*")
+    // Filters
+    .eq("status", filter)
+    // .order(column,{ascending:true||descending:true})
+    .order(columnToSort, directionObj);
+
+  // .gte(column, sort)
+  // .lte(column, sort);
+  if (error) throw new Error(error);
+  return data;
+}
+export function useFilterMovements({ filter, columnToSort, directionObj }) {
+  const [searchParams] = useSearchParams();
+  const { data, isLoading } = useQuery({
+    queryFn: () => filterMovements({ filter, columnToSort, directionObj }),
+    queryKey: ["movements", searchParams.get("filter")],
+  });
+  return { data, isLoading };
+}
+// async function sortMovements(sort) {
+//   let { data, error } = await supabase
+//     .from("movements")
+//     .select("*")
+
+//     .order(, sort);
+//   if (error) throw new Error(error);
+//   return data;
+// }
+// export function useSortMovements(sort) {
+//   const [searchParams] = useSearchParams();
+//   const { data, isLoading } = useQuery({
+//     queryFn: () => sortMovements(sort),
+//     queryKey: ["movements", searchParams.get("sort")],
+//   });
+//   return { data, isLoading };
+// }
