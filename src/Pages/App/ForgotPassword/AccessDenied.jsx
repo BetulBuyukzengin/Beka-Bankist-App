8d0d1ed112195b033a6db4a651c669b5880e0289
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-09-03 22:16:58 +0300

    feat: create access denied comp

---
+++ src/Pages/App/ForgotPassword/AccessDenied.jsx
@@ -0,0 +1,61 @@
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Grid, Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+function AccessDenied() {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      style={{
+        height: "100dvh",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+      }}
+    >
+      <Paper
+        elevation={1}
+        sx={{
+          p: "2rem 4rem ",
+          width: "50%",
+          m: "1rem",
+          backgroundColor: "transparent",
+          //   display: "flex",
+        }}
+      >
+        <Grid
+          container
+          gap={1}
+          sx={{
+            display: "flex",
+            width: "100%",
+          }}
+        >
+          <Grid item xs={12}>
+            <ArrowBackIcon onClick={() => navigate("/forgotPassword")} />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            style={{
+              fontWeight: "bold",
+              fontSize: "1rem",
+              //   alignSelf: "center",
+              //   textAlign: "center",
+            }}
+          >
+            You don &apos;t have permission to access this form !<br /> Write
+            your e-mail address in the I forgot my password form and submit it.
+            You can reset your password via the link sent to your e-mail.
+            {/* Bu forma erişim izniniz yok!Şifremiunuttum formuna e-posta adresinizi yazın ve gönderin. E-postanıza
+            gönderilen linkten şifrenizi sıfırlayabilirsiniz. */}
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
+
+export default AccessDenied;
