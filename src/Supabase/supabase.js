3422122b14600a3a76e549d5f928e8cec0675048
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-09-19 23:54:42 +0300

    feat : add environment variables

--- src/Supabase/supabase.js
+++ src/Supabase/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://zztdmxtwnkykekxoupjl.supabase.co";
-export const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6dGRteHR3bmt5a2VreG91cGpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1MjIzMzksImV4cCI6MjAyODA5ODMzOX0.FhMSrqvQWwLseWLZhwEla2fXYoJfb85RONOoLcK_j1U";
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
